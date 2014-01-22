87d1fd446ed8dd7fbf07e3825b4a5f5393ffdda8
Lucy Bain <lucybain@gmail.com>
2014-01-22 20:04:32 +1100

    VERY simple home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 LifeGoals::Application.routes.draw do
+  root :to => 'content#home'
   resources :goals
   resources :repeats
+  resources :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
